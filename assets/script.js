227473bd22a12e672849fa98037b0ae70e573f32
EduardoLeao-system <carloseduardomeirelesleao@gmail.com>
2025-04-16 16:26:10 -0400

    Ajustes na sidebar: espaçamento, hover e tamanho dos ícones/imagens

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,17 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            
             logoPath: '../assets/images/logo.png',
-            isSidebarVisible: true
+            isSidebarVisible: true,
+            menuItems: [
+                { icon: 'fas fa-shopping-cart', text: 'Compras' },
+                { icon: 'fas fa-rocket', text: 'Lançamentos' },
+                { icon: 'fas fa-chart-bar', text: 'Relatórios' },
+                { icon: 'fas fa-gift', text: 'Presentes' },
+                { icon: 'fas fa-user', text: 'Perfil' },
+                { icon: 'fas fa-cog', text: 'Configurações' },
+                { icon: 'fas fa-map-marker-alt', text: 'Localização' }
+            ]
         };
     },
     methods: {
